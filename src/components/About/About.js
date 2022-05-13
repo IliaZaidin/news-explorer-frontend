f94eac5b56769bce8167c051223e96c1628b351e
iliazaidin <ilia.zaid@gmail.com>
2022-05-13 17:42:05 +0300

    fix: about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,12 @@ function About() {
         <div className='about__text-wrapper'>
           <h2 className='about__title'>About the author</h2>
           <p className='about__subtitle'>
-            This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
-            <br/> <br/>
-            You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.
+            Hello everybody. My name is Ilia. This page is one of the projects on my way to becoming a Web Developer.
+            Almost a year ago, I decided to make a change in my life. That required of me to step out of my comfort
+            zone and to work really hard to pursue my dream. The road was hard. Several times along the way I
+            considered quitting, but finally I successfully completed the Web Development Training Program by Yandex.
+            During the course I studied the following technologies: HTML, CSS, JavaScript, React.JS, Node.JS and much more.
+            Now I am looking forward to starting my new career.
           </p>
           {/* <p className='about__subtitle'></p> */}
         </div>
